ab628cad898b44c29e7f274bda4076b6f3eea093
RonaldJongeneel <ronaldjongeneel@gmail.com>
2015-03-21 17:46:29 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Below is an R function to cache the potentially time-consuming computation of an inverse.
+# Then, if the contents of a vector are not changed, the inverse may be looked up in stead of recomputed.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix does exactly what the name implies: it makes a special object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    RonJoninv <- NULL
+    set <- function(y) {
+        x <<- y
+        RonJoninv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) RonJoninv <<- inverse
+    getinverse <- function() RonJoninv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
-
+# cacheSolve gives you the inverse of the matrix.
+# If it has been computed before, it will return that result and will not compute it again.
+# Otherwise, it computes the inverse, and caches the result for future use.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    RonJoninv <- x$getinverse()
+    if (!is.null(RonJoninv)) {
+        message("getting cached data.")
+        return(RonJoninv)
+    }
+    data <- x$get()
+    RonJoninv <- solve(data)
+    x$setinverse(RonJoninv)
+    RonJoninv
 }
